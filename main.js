15db01f8bc674be3033b27f260eaf6ed6d600cf7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2018-06-06 14:17:55 -0500

    Replaced failure values with errors, new version: 1.1.1.

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ const {Readable} = require('stream');
 
 const io = require('./node');
 
+// replace errors
+class FailedIO extends Error {
+	constructor(xhr, options, event, message = 'Failed I/O') {
+		super(message);
+		this.name = this.constructor.name;
+		Error.captureStackTrace(this, this.constructor);
+		this.xhr = xhr;
+		this.options = options;
+		this.event = event;
+	}
+};
+io.FailedIO = FailedIO;
+class TimedOut extends FailedIO {
+	constructor(xhr, options, event) {
+		super(xhr, options, event, 'Timed out I/O');
+	}
+};
+io.TimedOut = TimedOut;
+class BadStatus extends FailedIO {
+	constructor(xhr, options, event) {
+		super(xhr, options, event, 'Bad status I/O' + (xhr && xhr.status ? ': ' + xhr.status : ''));
+	}
+};
+io.BadStatus = BadStatus;
+
 io.node.attach();
 
 const passThrough = (_1, _2, data) => new io.Ignore(data);
